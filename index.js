fde6f21882cb7049133796e5004cd8bca2a39358
HACK WITH SURAJ <hackwithsuraj@gmail.com>
2024-09-04 17:57:50 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,51 @@
+// JavaScript Arithmetic Operators
+// Arithmetic operators are used to perform arithmetic between variables and/or values.
+
+// Addition
+let y = 5
+let x = y+2
+console.log(`Addition by 2 if y = 5 then x = ${x}`);
+
+
+// subtraction
+let a = 5
+let b = a-2
+console.log(`subtraction by 2 if a = 5 then b = ${b}`);
+
+
+// multiply
+let d = 5
+let c = d*2
+console.log(`multiply by 2 if d = 5 then c = ${c}`);
+
+
+// division
+let n = 5
+let m = n/2
+console.log(`division by 2 if n = 5 then m = ${m}`);
+
+
+// reminder
+let g = 5
+let h = g%2
+console.log(`reminder by 2 if g = 5 then h = ${h}`);
+
+
+// Incrementing
+let f = 5;
+f++;
+let z = f;
+console.log(`Incrementing by 1 if f = 5 then z = ${z}`);
+
+
+// Decrementing
+let o = 5;
+o--;
+let p = o;
+console.log(`Decrementing by 1 if o = 5 then p = ${p}`);
+
+
+// Exponentiation
+let s = 5;
+let l = o**3;
+console.log(`Exponentiation by 2 if s = 5 then l = ${l}`);
