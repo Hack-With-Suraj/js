4bc09e093fd4ba64afd9cb155d255ec39f3a929f
HACK WITH SURAJ <hackwithsuraj@gmail.com>
2024-09-29 15:23:17 +0530

    Add files via upload

---
+++ mehul.js
@@ -0,0 +1,48 @@
+// DATA TYPE IN JAVA SCRIPT
+
+// let number=-10
+// let text="hello"
+// let istrue=true
+// let person={ name:"bob"}
+// let numberarray=[1,2,3]
+// let emptyvalue=null
+// let uninitializedvariable 
+
+// console.log(number);
+// console.log(text);
+// console.log(istrue);
+// console.log(person);
+// console.log(numberarray);
+// console.log(emptyvalue);
+// console.log(uninitializedvariable);
+
+
+
+
+// let num1=10
+// let num2=num1
+// num1=20
+// console.log(num1);
+// console.log(num2);
+
+
+// let obj1={ name:"Alice"}
+// let obj2= obj1
+// obj1.name="bob"
+// console.log(obj1);
+// console.log(obj2);
+
+let a=10
+let b= 3
+let sum= a+b
+let difference= a-b
+let product = a*b
+let quotient=a/b
+let remainder =a%b
+
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
