706f0baeeedd69e4e4a050be4c6975c24d63b750
HACK WITH SURAJ <hackwithsuraj@gmail.com>
2024-09-29 15:25:52 +0530

    Add files via upload

---
+++ zoya.js
@@ -0,0 +1,49 @@
+// let number = 10;
+// let text = "hello";
+// let isTrue = true;
+// let person = { name: "Bob" };
+// let numberArray =[1,2,3];
+// let emptyValue = null;
+// let uninitialzedVariable;
+ 
+
+// console.log(typeof number);// Output: "number"
+// console.log(typeof text);// Output: "string"
+// console.log(typeof isTrue);// Output: "boolean"
+// console.log(typeof person);// Output: "object"
+// console.log(typeof numberArray);// Output: "object"
+// console.log(typeof emptyValue);// Output: "object"
+// console.log(typeof uninitialzedVariable);// Output: "undefined"
+
+
+// Primitive data type (number)
+let num1 = 10;
+let num2 = num1;
+num1 = 20;
+console.log(num1);// Output: 20
+console.log(num2);// Output: 10
+
+// Non-primitive data type (object)
+let obj1 = { name:"Alice"};
+let obj2 = obj1;
+obj1.name = "Bob";
+console.log(num1);// Output: (name:"Bob")
+console.log(num2);// Output: (name:"Bob")
+
+
+
+
+let a = 10;
+let b = 3;
+let sum = a+b;//13
+let difference =a-b;//7
+let product =a*b;//30
+let quotient = a/b;//3.333333333333335
+let remainder = a%b ; //1
+let power = a** b;//1000
+
+
+
+
+
+
