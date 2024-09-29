0f5aea9dcafdb9283add02da56b05791e0a2a1d0
HACK WITH SURAJ <hackwithsuraj@gmail.com>
2024-09-29 15:21:20 +0530

    Add files via upload

---
+++ kaushal.js
@@ -0,0 +1,53 @@
+//DATA TYPES IN JAVASCRIPT :
+
+
+// let  number = 10
+// let text = "Hello"
+// let isTrue = true
+// let person = { name: "Bob"}
+// let numberArray = [1, 2, 3]
+// let emptyValue = null;
+// let uninitializedVariable
+
+
+
+// console.log(number);
+// console.log(text);
+// console.log(isTrue);
+// console.log(person);
+// console.log(numberArray);
+// console.log(emptyValue);
+// console.log(uninitializedVariable);
+
+
+
+// let num1 = 10
+// let num2 = num1
+// num1 = 20
+// console.log(num1);
+// console.log(num2);
+
+
+
+// let obj1 = { name: "Alice"}
+// let obj2 = obj1
+// obj1.name = "bob"
+// console.log(obj1);
+// console.log(obj2);
+
+
+
+let a = 10
+let b = 3
+let sum = a+b
+let difference = a-b
+let product = a*b
+let quotient = a/b
+let remainder = a%b
+
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
