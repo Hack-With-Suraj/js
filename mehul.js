ec29599874cad28791c6fc0ec826cca3aa875c13
HACK WITH SURAJ <hackwithsuraj@gmail.com>
2024-10-01 18:07:33 +0530

    Update mehul.js

--- mehul.js
+++ mehul.js
@@ -1,6 +1,7 @@
 // DATA TYPE IN JAVA SCRIPT
 
-// let number=-10
+const number= 10
+
 // let text="hello"
 // let istrue=true
 // let person={ name:"bob"}
@@ -20,13 +21,14 @@
 
 
 // let num1=10
+
 // let num2=num1
 // num1=20
 // console.log(num1);
 // console.log(num2);
 
 
-// let obj1={ name:"Alice"}
+// let obj1={ name:"mehul"}
 // let obj2= obj1
 // obj1.name="bob"
 // console.log(obj1);
@@ -41,8 +43,65 @@ let quotient=a/b
 let remainder =a%b
 
 
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
-console.log(a%b);
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+
+// greater than less than in js
+
+// console.log(11>10);
+// console.log(10<12);
+// console.log(20<=20);
+// console.log(100>=100);
+
+// if else in js 
+
+// let age=17
+// if (age >= 18) {
+//   console.log("you are elegable to vote");
+// } else {
+//     console.log("you are not elegable to vote");
+// }
+
+// 90 >= very good 
+// 80 >= good 
+// 70 >= normal
+// 60 >= very normal
+ 
+// Nested if else in js
+
+// let marks=60
+
+
+// if (marks >= 90 ) {
+//     console.log("very good");
+// } else if(marks >= 80 ){
+//     console.log("good");
+// }
+// else if(marks >= 70){
+//     console.log("normal");
+// }
+// else if( marks >=60){
+//     console.log("very normal");
+// }
+
+
+
+let num1 = 4
+let num2 = 5
+
+let cal = "div"
+
+if (cal == "add"){
+    console.log(num1+num2);
+}else if(cal == "sub"){
+    console.log(num1-num2);
+}else if(cal =="mul"){
+    console.log(num1*num2);
+}
+else if(cal=="div"){
+    console.log(num1/num2);
+    
+}
