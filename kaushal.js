ff2978d3a11659c4519efecc65fb6ce9f5bab802
HACK WITH SURAJ <hackwithsuraj@gmail.com>
2024-10-01 18:05:48 +0530

    Update kaushal.js

--- kaushal.js
+++ kaushal.js
@@ -37,17 +37,82 @@
 
 
 
-let a = 10
-let b = 3
-let sum = a+b
-let difference = a-b
-let product = a*b
-let quotient = a/b
-let remainder = a%b
+// let a = 10
+// let b = 3
+// let sum = a+b
+// let difference = a-b
+// let product = a*b
+// let quotient = a/b
+// let remainder = a%b
 
 
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
-console.log(a%b);
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+
+
+// IF - ELSE WITH EXAMPLE :
+
+// let age = 18
+// if (age >= 18) {
+// console.log("you are eligible to vote");
+// } else {
+//     console.log("you are not eligible to vote");  
+// }
+
+
+
+// NESTED IF - ELSE WITH EXAMPLE :
+
+// let grade = 85
+
+// if (grade >= 90) {
+//     console.log("exellent");
+    
+// } else if (grade >= 80){
+//     console.log("very good");
+    
+// } else if (grade >= 70) {
+//     console.log("good");
+    
+// } else {
+//     console.log("needs improvement");
+    
+// }
+
+
+// let number = 10
+
+// if (number > 0){
+//     console.log("positive");
+    
+// } else if (number < 0){
+//     console.log("negetive");
+    
+// } else {
+//     console.log("zero");
+    
+// }
+
+
+// let num1 = 3
+// let num2 = 3
+// let cal = "divide"
+
+// if( cal == "add"){
+// console.log(num1 + num2)
+// } else if (cal == "subtract") {
+//     console.log(num1 - num2);
+    
+// } else if (cal == "multiply") {
+//     console.log(num1 * num2);
+    
+// } else if (cal == "divide") {
+//     console.log(num1 / num2);
+    
+// } else if (cal == "reminder") {
+//     console.log(num1 % num2);
+    
+// }
