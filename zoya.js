1dbf58b0ca92b530bc381db507fceccb46268668
HACK WITH SURAJ <hackwithsuraj@gmail.com>
2024-10-06 15:15:02 +0530

    Update zoya.js

--- zoya.js
+++ zoya.js
@@ -17,33 +17,87 @@
 
 
 // Primitive data type (number)
-let num1 = 10;
-let num2 = num1;
-num1 = 20;
-console.log(num1);// Output: 20
-console.log(num2);// Output: 10
+// let num1 = 10;
+// let num2 = num1;
+// num1 = 20;
+// console.log(num1);// Output: 20
+// console.log(num2);// Output: 10
 
 // Non-primitive data type (object)
-let obj1 = { name:"Alice"};
-let obj2 = obj1;
-obj1.name = "Bob";
-console.log(num1);// Output: (name:"Bob")
-console.log(num2);// Output: (name:"Bob")
+// let obj1 = { name:"Alice"};
+// let obj2 = obj1;
+// obj1.name = "Bob";
+// console.log(num1);// Output: (name:"Bob")
+// console.log(num2);// Output: (name:"Bob")
 
 
 
 
-let a = 10;
-let b = 3;
-let sum = a+b;//13
-let difference =a-b;//7
-let product =a*b;//30
-let quotient = a/b;//3.333333333333335
-let remainder = a%b ; //1
-let power = a** b;//1000
+// let a = 10;
+// let b = 3;
+// let sum = a+b;//13
+// let difference =a-b;//7
+// let product =a*b;//30
+// let quotient = a/b;//3.333333333333335
+// let remainder = a%b ; //1
+// let power = a** b;//1000
 
 
+// IF - ELSE WITH EXAMPLE :
+
+// let age = 17
+// if (age >= 18) {
+// console.log("you are eligible to vote");
+// } else {
+//     console.log("you are not eligible to vote");  
+// }
 
 
+// NESTED IF - ELSE WITH EXAMPLE :
+
+// let grade = 64
+
+// if (grade >= 90) {
+//     console.log("exellent");
+    
+// } else if (grade >= 80){
+//     console.log("very good");
+    
+// } else if (grade >= 70) {
+//     console.log("good");
+    
+// } else {
+//     console.log("needs improvement");
+    
+// }
 
 
+// let num1 = 3
+// let num2 = 9
+// let cal = "add"
+
+// if( cal == "add"){
+// console.log(num1 + num2)
+// } else if (cal == "subtract") {
+//     console.log(num1 - num2);
+    
+// } else if (cal == "multiply") {
+//     console.log(num1 * num2);
+    
+// } else if (cal == "divide") {
+//     console.log(num1 / num2);
+    
+// } else if (cal == "reminder") {
+//     console.log(num1 % num2);
+    
+// }
+
+// for (let i = 0;  i < 10000000000001; i++) {
+// console.log(i);
+// }
+
+// let i = 0
+// while (i<11) {
+// console.log(i);
+//     i++
+// }
